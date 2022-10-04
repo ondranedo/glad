0c4a52ad083bd9ce38fcf41587252f47c7192caa
ondranedo <nedojedly.o@gmail.com>
2022-10-04 23:28:36 +0200

    fixing for cmake

--- gl_4.0_core_debug/include/glad/gl.h
+++ gl_4.0_core_debug/include/glad/gl.h
@@ -1071,7 +1071,7 @@ typedef void (*GLADpostcallback)(void *ret, const char *name, GLADapiproc apipro
 #define GL_ZERO 0
 
 
-#include <KHR/khrplatform.h>
+#include "../include/KHR/khrplatform.h"
 typedef unsigned int GLenum;
 typedef unsigned char GLboolean;
 typedef unsigned int GLbitfield;
